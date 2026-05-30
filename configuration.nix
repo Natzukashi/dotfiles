b31b6984f6619060765c8d916a906f3a577b411c
Parijat <parijatbasak06@gmail.com>
2026-05-30 12:45:10 +0530

    Initial commit. Hello, NixOS!

---
+++ configuration.nix
@@ -0,0 +1,183 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 7d";
+  };
+
+  networking = {
+    hostName = "tsuwabuki";
+    networkmanager.enable = true;
+  };
+
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+  };
+
+  time.timeZone = "Asia/Kolkata";
+  i18n.defaultLocale = "en_IN";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_IN";
+    LC_IDENTIFICATION = "en_IN";
+    LC_MEASUREMENT = "en_IN";
+    LC_MONETARY = "en_IN";
+    LC_NAME = "en_IN";
+    LC_NUMERIC = "en_IN";
+    LC_PAPER = "en_IN";
+    LC_TELEPHONE = "en_IN";
+    LC_TIME = "en_IN";
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  services.xserver.enable = true;
+
+  services.power-profiles-daemon.enable = false;
+  services.auto-cpufreq = {
+    enable = true;
+    settings = {
+      battery = {
+        governor = "powersave";
+        turbo = "never";
+      };
+      charger = {
+        governor = "performance";
+        turbo = "auto";
+      };
+    };
+  };
+
+  services.desktopManager.plasma6.enable = true;
+  services.displayManager.plasma-login-manager.enable = true;
+
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  services.printing.enable = true;
+  services.flatpak.enable = true;
+  services.tailscale.enable = true;
+
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  users.users.dog = {
+    isNormalUser = true;
+    description = "Dog";
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+    ];
+  };
+
+  hardware.graphics.enable = true;
+  services.xserver.videoDrivers = [
+    "amdgpu"
+    "nvidia"
+  ];
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+    open = true;
+    nvidiaSettings = true;
+
+    prime = {
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
+      amdgpuBusId = "PCI:5:0:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
+  };
+
+  environment.plasma6.excludePackages = with pkgs.kdePackages; [
+    konsole
+    kate
+  ];
+
+  programs = {
+    firefox.enable = true;
+    starship.enable = true;
+    git.enable = true;
+    nix-ld.enable = true;
+    steam.enable = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    neovim
+    vim
+    wget
+    alacritty
+    gcc
+    curl
+    gnumake
+    lua-language-server
+    pyright
+    nixd
+    tree-sitter
+    nixfmt
+    wl-clipboard
+    fastfetch
+    (lutris.override {
+      buildFHSEnv =
+        args:
+        pkgs.buildFHSEnv (
+          args
+          // {
+            multiPkgs =
+              envPkgs:
+              let
+                originalPkgs = args.multiPkgs envPkgs;
+                customLdap = envPkgs.openldap.overrideAttrs (_: {
+                  doCheck = false;
+                });
+              in
+              builtins.filter (p: (p.pname or "") != "openldap") originalPkgs ++ [ customLdap ];
+          }
+        );
+    })
+    papirus-icon-theme
+    p7zip
+    unrar
+    unzip
+    motrix-next
+    tealdeer
+    file
+    wine
+    kdePackages.bluedevil
+  ];
+
+  fonts.packages = with pkgs; [
+    nerd-fonts.intone-mono
+    nerd-fonts.jetbrains-mono
+  ];
+
+  system.stateVersion = "26.05";
+}
---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "tsuwabuki";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+    }:
+    {
+      nixosConfigurations.tsuwabuki = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+        ];
+      };
+    };
+}
