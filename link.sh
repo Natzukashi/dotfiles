0cc1bafab85953aea617823a1cf94da9f9978189
Parijat Basak <parijatbasak06@gmail.com>
2026-08-01 17:15:46 +0530

    Goodbye alacritty...
    If I knew alacritty was so focused on minimalism and speed that it
    completely butchered cosmetics... maybe I won't have used it for so
    long.

--- link.sh
+++ link.sh
@@ -6,7 +6,7 @@ CONFIG="$HOME/.config"
 # map: source (relative to ~/dotfiles) -> target
 declare -A links=(
     ["hypr"]="$CONFIG/hypr"
-    ["alacritty"]="$CONFIG/alacritty"
+    ["kitty"]="$CONFIG/kitty"
     ["zshrc"]="$HOME/.zshrc"
     ["clang-format"]="$HOME/.clang-format"
 )
